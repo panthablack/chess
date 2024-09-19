8352f6d51f3673287e0368379e89829505bd3661
James Randall <james@manmachineltd.com>
2024-09-19 18:38:30 +1000

    updated border styling

--- src/components/boards/StandardCheckeredBoard.vue
+++ src/components/boards/StandardCheckeredBoard.vue
@@ -3,13 +3,17 @@
     class="standardCheckeredBoardContainer flex justify-center items-center p-12 m-12 gap-0 flex-nowrap flex-shrink flex-grow-0"
   >
     <div
-      class="boardWrapper shadow-lg flex flex-initial text-center justify-center items-center p-0 m-0 gap-0 flex-wrap flex-col"
+      class="boardWrapper rounded-lg bg-yellow-800 border-yellow-800 border-8 shadow-lg flex flex-initial text-center justify-center items-center p-4 m-0 gap-0 flex-wrap flex-col"
     >
-      <Row
-        v-for="row in rows"
-        :key="'row' + row.id"
-        :row="row"
-      />
+      <div
+        class="outlineWrapper rounded-md border-8 bg-opacity-90 border-opacity-70 bg-yellow-800 border-yellow-950 shadow-md"
+      >
+        <Row
+          v-for="row in rows"
+          :key="'row' + row.id"
+          :row="row"
+        />
+      </div>
     </div>
   </div>
 </template>
